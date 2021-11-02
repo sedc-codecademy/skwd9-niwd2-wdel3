bf1121bf36a2874328f24a4cb617f40913b564a0
George <dimitrov.gjorge@protonmail.com>
2021-11-02 20:46:29 +0100

    Class 06 added

--- Class_06-First-React-App-Continue/src/components/Body.jsx
+++ Class_06-First-React-App-Continue/src/components/Body.jsx
@@ -1,43 +1,122 @@
-import React from "react";
+import React, { Component } from "react";
+import { v4 as uuid } from "uuid"; // Library to generate UNIQUE IDS for us
+// V1 Generates and timestamp
+
 import BigMovie from "./BigMovie";
 import "./Body.scss";
 
-const Body = (props) => {
-  const { children } = props;
-  console.log(props.randomText);
+class Body extends Component {
+  constructor(props) {
+    super(props);
 
-  const mainMovies = [
-    {
-      id: 1,
-      name: "Harry Potter and the half blood prince",
-      releaseDate: "15 July 2009",
-      director: "David Yates",
-    },
-    {
-      id: 2,
-      name: "The Hobbit: Battle of the five armies",
-      releaseDate: "15 Dec 2014",
-      director: "Peter Jackson",
-    },
-  ];
+    this.state = {
+      mainMovies: [
+        {
+          id: 1,
+          name: "Harry Potter and the half blood prince",
+          releaseDate: "15 July 2009",
+          director: "David Yates",
+        },
+        {
+          id: 2,
+          name: "The Hobbit: Battle of the five armies",
+          releaseDate: "15 Dec 2014",
+          director: "Peter Jackson",
+        },
+      ],
 
-  return (
-    <>
-      <div>{children}</div>
-      <div className="container">
-        <h1>Movie list:</h1>
+      name: "",
+      releaseDate: "",
+      director: "",
+    };
+  }
 
-        {mainMovies.map((movie) => (
-          <BigMovie
-            key={movie.id}
-            movieName={movie.name}
-            releaseDate={movie.releaseDate}
-            director={movie.director}
-          />
-        ))}
-      </div>
-    </>
-  );
-};
+  //FIRST APPROACH
+  handleMovieName = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleReleaseDate = (e) => {
+    this.setState({
+      releaseDate: e.target.value,
+    });
+  };
+
+  handleMovieDirector = (e) => {
+    this.setState({
+      director: e.target.value,
+    });
+  };
+
+  handleAddNewMovie = (e) => {
+    e.preventDefault();
+
+    const newMovie = {
+      id: uuid(),
+      name: this.state.name,
+      releaseDate: this.state.releaseDate,
+      director: this.state.director,
+    };
+
+    console.log(newMovie);
+
+    this.setState({
+      mainMovies: [...this.state.mainMovies, newMovie],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>{this.props.children}</div>
+        <div className="container">
+          <div>
+            <h1>Movie list:</h1>
+            {this.state.mainMovies.map((movie) => (
+              <BigMovie
+                key={movie.id}
+                movieName={movie.name}
+                releaseDate={movie.releaseDate}
+                director={movie.director}
+              />
+            ))}
+          </div>
+          <div>
+            <h1>Add new movie</h1>
+            <label htmlFor="movieName">Movie name:</label>
+            <input
+              onChange={this.handleMovieName}
+              value={this.state.name}
+              name="movieName"
+              type="text"
+            />
+
+            <label htmlFor="releaseDate">Release date:</label>
+            <input
+              onChange={this.handleReleaseDate}
+              value={this.state.releaseDate}
+              name="releaseDate"
+              type="text"
+            />
+
+            <label htmlFor="director">Director:</label>
+            <input
+              onChange={this.handleMovieDirector}
+              value={this.state.director}
+              name="director"
+              type="text"
+            />
+
+            <button onClick={(e) => this.handleAddNewMovie(e)}>
+              Add Movie
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Body;
