83ab85494248d522acaf3c6ec0ff207a902722a3
George <dimitrov.gjorge@protonmail.com>
2021-11-09 18:31:06 +0100

    our store app

---
+++ our-store-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Playground } from "./components/playground/Playground";
+
+//REACT HOOKS;
+
+function App() {
+  return (
+    <div>
+      <Playground />
+    </div>
+  );
+}
+
+export default App;
